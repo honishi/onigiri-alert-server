3119fab7f7a0031ee5c4a66c86957853415a7efc
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-04-07 01:28:56 +0900

    Updated push.sh to support sub user.

--- utility/push.sh
+++ utility/push.sh
@@ -3,37 +3,67 @@
 set -e
 
 basedir=$(cd $(dirname $0);pwd)
-envfile=${basedir}/push.env
+# envfile=${basedir}/push.env
 
-if [ $# -lt 1 ]; then
-  echo "usage:   $0 channel_name [env_file]"
-  echo "example: $0 ts02 ./push.env.dev"
+# echo arguments count: $#
+# exit
+
+if ! [ $# -eq 2 -o $# -eq 3 ]; then
+  echo "usage:   \$ $0 envfile channel [subuser]"
+  echo "example: \$ $0 ./push.env.dev ts02"
+  echo "         \$ $0 ./push.env.dev ts02 user1"
   exit 1
 fi
 
-if [ $# -eq 2 ]; then
-  envfile=${2}
+# envfile, channel, subuser
+
+envfile=${1}
+channel=${2}
+subuser=${3:-""}
+
+# if [ $# -eq 3 ]; then
+#   subuser=", \"${3}\": true"
+# fi
+
+echo "-----"
+echo "envfile: ${envfile}"
+echo "channel: ${channel}"
+echo "subuser: ${subuser}"
+
+# where
+
+where="\"channels\": \"${channel}\""
+
+if [ ! -z ${subuser} ]; then
+  where="${where}, \"${3}\": true"
 fi
 
-echo using envfile ${envfile}
-source ${envfile}
+echo "-----"
+echo "where: ${where}"
+
+# expire time
 
 current_time=$(date '+%s')
 active_duration=$((60 * 60))  # = 1h
 expire_time=$((${current_time} + ${active_duration}))
 
+echo "-----"
 echo "current: ${current_time}"
 echo "active:  ${active_duration}"
 echo "expire:  ${expire_time}"
+
+echo "-----"
+
+source ${envfile}
 
 curl -X POST \
   -H "X-Parse-Application-Id: ${APPLICATION_ID}" \
   -H "X-Parse-REST-API-Key: ${REST_API_KEY}" \
   -H "Content-Type: application/json" \
   -d "{
-        \"channels\": [
-          \"${1}\"
-        ],
+        \"where\": { 
+          ${where}
+        },
         \"data\": {
           \"alert\": \"テストアラートです.\",
           \"sound\": \"horagai.aiff\"
